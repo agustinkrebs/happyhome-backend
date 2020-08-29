19dff8e4503a8f9c75ee4b76e3b971606247cc88
AGUSTIN KREBS <akrebs2@uc.cl>
2020-08-29 13:37:01 -0400

    scaffold of shopping_list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_173236) do
+ActiveRecord::Schema.define(version: 2020_08_29_173624) do
 
   create_table "family_users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_08_29_173236) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_family_users_on_user_id"
+  end
+
+  create_table "shopping_lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
